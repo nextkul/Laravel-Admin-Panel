e176a56c704018f239f4fe4c2a676470bca429ad
Ashwini Upadhyay <nextkul.info@gmail.com>
2022-01-04 22:40:52 +0530

    updated admin header

--- resources/views/admin/include/header.blade.php
+++ resources/views/admin/include/header.blade.php
@@ -119,6 +119,14 @@
           <i class="fas fa-th-large"></i>
         </a>
       </li>
+      <li class="nav-item dropdown">
+      
+            <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">{{ config('settings.site.header.name') }}</a>
+            <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
+              <li><a href="#" class="dropdown-item">Profile</a></li>
+              <li><a href="#" class="dropdown-item">Settings</a></li>
+            </ul>
+          </li>
     </ul>
   </nav>
   <!-- /.navbar -->
